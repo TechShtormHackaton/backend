c3230fcd65a7fe55405a8560ff4cc5ea0e8b6f6a
Mekan777-alt <mekan2678@gmail.com>
2024-08-23 22:28:36 +0300

    add Docker and docker compose file

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
 from controllers.load_file_controllers import router as LoadFileController
 
 
 app = FastAPI()
 
 app.include_router(LoadFileController)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
